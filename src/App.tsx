bd1495dbd7e3af419f3d0fddf7caa8d8b04909f4
Mavo Sam <mavosam4@gmail.com>
2026-02-15 14:45:17 +0300

    Successfully ran the project locally

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import About from "./pages/About";
-import Academics from "./pages/Academics";
-import Staff from "./pages/Staff";
-import Blog from "./pages/Blog";
-import Events from "./pages/Events";
-import Admissions from "./pages/Admissions";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
+import Index from "@/pages/Index";
+import About from "@/pages/About";
+import Academics from "@/pages/Academics";
+import Staff from "@/pages/Staff";
+import Blog from "@/pages/Blog";
+import Events from "@/pages/Events";
+import Admissions from "@/pages/Admissions";
+import Contact from "@/pages/Contact";
+import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
 
